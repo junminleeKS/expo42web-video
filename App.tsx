b4d9a68e5c66ed83d897b643be2211bdb37862fc
Jun-Min Lee <junmin@kontratechnologies.com>
2021-07-08 17:20:52 -0600

    added video demo

--- App.tsx
+++ App.tsx
@@ -1,21 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { Video } from "expo-av";
+import * as React from "react";
+import { Button, StyleSheet, View } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+export default function App() {
+  const [videoVisible, setVideoVisible] = React.useState(false);
+  return (
+    <View style={styles.container}>
+      {videoVisible && (
+        <Video
+          resizeMode="contain"
+          source={{
+            uri: "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+          }}
+          useNativeControls
+        />
+      )}
+      <View>
+        <Button
+          title={videoVisible ? "Hide" : "Show"}
+          onPress={() => setVideoVisible(!videoVisible)}
+        />
+      </View>
+    </View>
+  );
+}
